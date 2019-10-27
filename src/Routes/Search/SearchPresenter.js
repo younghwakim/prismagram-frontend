f8fed58a3c515861ba3d50a626407d1e8cb65e35
younghwakim <younghwa.kym@gmail.com>
2019-10-27 23:33:17 +0900

    #8.4 SquarePost Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,55 +4,65 @@ import PropTypes from "prop-types";
 import FatText from "../../Components/FatText";
 import Loader from "../../Components/Loader";
 import UserCard from "../../Components/UserCard";
+import SquarePost from "../../Components/SquarePost";
 
 const Wrapper = styled.div`
-    height: 50vh;
-    text-align: center;
+  height: 50vh;
+  /* text-align: center; */
 `;
 
 const Section = styled.div`
-    margin-bottom: 50px;
-    display: grid;
-    grid-gap: 25px;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: 160px;
-    grid-auto-rows: 160px;
+  margin-bottom: 50px;
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(4, 160px);
+  grid-template-rows: 160px;
+  grid-auto-rows: 160px;
 `;
 
+const PostSection = styled(Section)`
+  grid-template-columns: repeat(4, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
+`;
+
+
 const SearchPresenter = ({ searchTerm, loading, data }) => {
-    if(searchTerm === undefined) {
-        return <Wrapper><FatText text={"조회 할 목록이 없습니다."} /></Wrapper>;
-    } else if(loading) {
-        return <Wrapper><Loader /></Wrapper>;
-    } else if(data && data.searchUser && data.searchPost) {
-        return (
-          <Wrapper>
-            <Section>
-              {data.searchUser.length === 0 ? (
-                <FatText text="검색된 사용자가 없습니다." />
-              ): data.searchUser.map(user =>
-              <UserCard
-                key={user.id}
-                username={user.username}
-                isFollowing={user.isFollowing}
-                url={user.avatar}
-                isSelf={user.isSelf}
-                id={user.id}
-              />)}
-            </Section>
-            <Section>
-                {data.searchPost.length === 0 ? (
-                    <FatText text="검색된 이미지가 없습니다." />
-                ) : data.searchPost.map(post => null)}
-            </Section>
-          </Wrapper>
-        );
-    }
+  if(searchTerm === undefined) {
+      return <Wrapper><FatText text={"조회 할 목록이 없습니다."} /></Wrapper>;
+  } else if(loading) {
+      return <Wrapper><Loader /></Wrapper>;
+  } else if(data && data.searchUser && data.searchPost) {
+      return (
+        <Wrapper>
+          <Section>
+            {data.searchUser.length === 0 ? (
+              <FatText text="검색된 사용자가 없습니다." />
+            ): data.searchUser.map(user =>
+            <UserCard
+              key={user.id}
+              username={user.username}
+              isFollowing={user.isFollowing}
+              url={user.avatar}
+              isSelf={user.isSelf}
+              id={user.id}
+            />)}
+          </Section>
+          <PostSection>
+              {data.searchPost.length === 0 ? (
+                  <FatText text="검색된 이미지가 없습니다." />
+              ) : data.searchPost.map(post =>
+                <SquarePost key={post.id} likeCount={post.likeCount} commentCount={post.commentCount} file={post.files[0]} />
+              )}
+          </PostSection>
+        </Wrapper>
+      );
+  }
 }
 
 SearchPresenter.propTypes = {
-    searchTerm: PropTypes.string,
-    loading: PropTypes.bool
+  searchTerm: PropTypes.string,
+  loading: PropTypes.bool
 }
 
 export default SearchPresenter;
